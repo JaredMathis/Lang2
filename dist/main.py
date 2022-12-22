580a6c0b621328ffa1f559c9601d64c61bcbb89b
JaredMathis <jared.mathis@native.consulting>
2022-12-22 08:11:59 -0500

    progress

--- dist/main.py
+++ dist/main.py
@@ -3,11 +3,21 @@ from browser import *
 def element_full_width(b):
     b.style.width = '100%'
 
-def element_button(text):
+def element_button(text, on_click):
     b = html.BUTTON(text)
     element_full_width(b)
+    b.bind("click", on_click)
     return b
 
-b = element_button('L1')
+languages = [{
+    "name": "Spanish"
+}]
 
-document <= b 
+language_chooser = html.DIV()
+document <= language_chooser 
+
+for l in languages:
+    def on_click_language(ev):
+        language_chooser["hidden"] = "true"
+    b = element_button(l["name"], on_click_language)
+    language_chooser <= b 
