f2bed12375c15c9cd243df867733f33a0524f4db
JaredMathis <jared.mathis@native.consulting>
2022-12-22 08:54:45 -0500

    progress

--- dist/main.py
+++ dist/main.py
@@ -12,32 +12,51 @@ def element_button(text, on_click):
 def element_chooser(choices, label_get, on_choice):
     chooser = html.DIV()
     for choice in choices:
-        def on_click_choice(ev):
-            chooser.remove()
-            on_choice(choice)
-        b = element_button(label_get(choice), on_click_choice)
+        def on_click_get(c=choice):
+            def on_click_choice(ev):
+                chooser.remove()
+                on_choice(c)
+            return on_click_choice
+        b = element_button(label_get(choice), on_click_get())
         chooser <= b
     return chooser
 
 def name_get(x):
     return x["name"]
 
+def print_me(x):
+    print(x)
+    return x
+
 def on_choose_language(ev):
     main_choices = [{
+        "name":"Home",
+        "choose": home
+    }, {
         "name":"Learn",
-    },{
+        "choose": on_learn
+    }, {
         "name":"Read",
     },]
     document <= element_chooser(
         main_choices, 
         name_get, 
-        lambda c:None)
+        lambda c:print_me(c)["choose"]())
+
+mistakes = []
+
+def on_learn():
+    print('here on learn')
+
 
 languages = [{
-    "name": "Spanish"
+    "name": "Spanish",
 }]
 
-document <= element_chooser(
-    languages, 
-    name_get, 
-    on_choose_language)
+def home():
+    document <= element_chooser(
+        languages, 
+        name_get, 
+        on_choose_language)
+
+home()
