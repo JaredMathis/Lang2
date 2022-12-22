674700d6bdb84d0448a30e0beda4f3a021efb19e
JaredMathis <jared.mathis@native.consulting>
2022-12-21 19:40:11 -0500

    2022-12-21 19:40:10.928899 python -m metapensiero.pj src/py/main.py -o src/js/main.js

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import {document_get} from 'py/document_get';
-var b, tag_name;
-tag_name = "b";
-b = document_get().createElement(tag_name);
-b.innerHTML = "hello";
-document_get().body.appendChild(b);
-
-//# sourceMappingURL=main.js.map
