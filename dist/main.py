182fc34ca41e60b884f88733b1727be8d15f0e1b
JaredMathis <jared.mathis@native.consulting>
2022-12-21 20:29:17 -0500

    progress

--- dist/main.py
+++ dist/main.py
@@ -1,2 +1,13 @@
-from browser import document
-document <= "Hello !"
+from browser import *
+
+def element_full_width(b):
+    b.style.width = '100%'
+
+def element_button(text):
+    b = html.BUTTON(text)
+    element_full_width(b)
+    return b
+
+b = element_button('L1')
+
+document <= b 
