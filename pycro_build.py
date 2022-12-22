48a8dfa0357a57887bd08b633b16634568fa3f05
JaredMathis <jared.mathis@native.consulting>
2022-12-21 19:38:25 -0500

    2022-12-21 19:38:25.813685 python -m metapensiero.pj src/py/main.py src/py/document_get.py -o src/js/main.js

--- pycro_build.py
+++ pycro_build.py
@@ -1,7 +1,7 @@
 from pycro_common import *
 
 
-system_command('python -m metapensiero.pj src/py/main.py -o src/js/main.js', git_acp)
+system_command('python -m metapensiero.pj src/py/main.py src/py/document_get.py -o src/js/main.js', git_acp)
 system_command('npx webpack ./src/js/main.js', git_acp)
 git_command(lambda:file_create_if_no_exists('./dist/index.html', """
 <html>
