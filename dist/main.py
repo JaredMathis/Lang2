498608e9db5ab236e6a6e78d3e4e5c70f5e72235
JaredMathis <jared.mathis@native.consulting>
2022-12-22 08:18:37 -0500

    progress

--- dist/main.py
+++ dist/main.py
@@ -9,15 +9,18 @@ def element_button(text, on_click):
     b.bind("click", on_click)
     return b
 
+def element_chooser(choices, label_get, on_choice):
+    chooser = html.DIV()
+    for choice in choices:
+        def on_click_language(ev):
+            chooser.remove()
+            on_choice(choice)
+        b = element_button(label_get(choice), on_click_language)
+        chooser <= b
+    return chooser
+
 languages = [{
     "name": "Spanish"
 }]
 
-language_chooser = html.DIV()
-document <= language_chooser 
-
-for l in languages:
-    def on_click_language(ev):
-        language_chooser["hidden"] = "true"
-    b = element_button(l["name"], on_click_language)
-    language_chooser <= b 
+document <= element_chooser(languages, lambda c:c["name"], lambda c:None)
