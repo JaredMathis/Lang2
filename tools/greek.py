1fc0dfbd586b44c5943eb3060de0cf00888942a6
JaredMathis <jared.mathis@native.consulting>
2022-12-28 10:09:04 -0500

    progress

--- tools/greek.py
+++ tools/greek.py
@@ -1,22 +1,24 @@
 import xml.etree.ElementTree as ET
 
+from common import file_json_write
+
 # Parse the XML document
 tree = ET.parse('tools/bible/interlinear/strongsgreek.xml')
 root = tree.getroot()
 
 # Iterate over the root element's children
-words = []
+words = {}
 for child in root:
     if child.tag == 'entries':
         for word in child:
             word_result = {}
-            words.append(word_result)
             for prop in word:
                 if prop.tag == "strongs":
-                    word_result["id"] = prop.text
+                    words[prop.text] = word_result
                 elif prop.tag == "strongs_def":
                     word_result["definition"] = prop.text.strip()
                 elif prop.tag == "greek":
                     word_result["word"] = prop.get("unicode")
                     word_result["transliteration"] = prop.get("translit")
-            print(word_result)
+
+file_json_write('bucket/translations/gr_en.json', words)
