4e22684d579304e501c99d6153f581f7a7c9dfed
JaredMathis <jared.mathis@native.consulting>
2022-12-28 11:47:00 -0500

    refactor

--- public/main.py
+++ public/main.py
@@ -1,8 +1,13 @@
 from browser import document
+from browser import html
 import json
+
+def button(parent, text):
+    parent <= html.BUTTON(text)
+
 
 file_path = "https://firebasestorage.googleapis.com/v0/b/wlj-lang.appspot.com/o/languages.json?alt=media&"
 languages = json.load(open(file_path))
 
 for l in languages:
-    document <= l["name"]
+    button(document, l["name"])
