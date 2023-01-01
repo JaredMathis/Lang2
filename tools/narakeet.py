e4d08972cfa12d822f6916ed44ee8cb6bf69b730
JaredMathis <jared.mathis@native.consulting>
2023-01-01 04:48:55 -0500

    rename

--- tools/narakeet.py
+++ tools/narakeet.py
@@ -1,7 +1,7 @@
 from tools.gcloud import file_path_audio
 
 
-def gcloud_tts(text, language_code):
+def narakeet_tts(text, language_code):
     file_name_string, file_name_string_exists = file_path_audio(text, language_code)
     if (file_name_string_exists):
         return
