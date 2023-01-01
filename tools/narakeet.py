cf6a2302ed8f5e9933c5be73e69f8787c1da5f24
JaredMathis <jared.mathis@native.consulting>
2023-01-01 05:00:53 -0500

    progress

--- tools/narakeet.py
+++ tools/narakeet.py
@@ -1,16 +1,22 @@
 from common import file_json_read
 from gcloud import file_path_audio
+import os
 
 api_key = file_json_read('./gitignore/narakeet-api-client.json')["api_key"]
 
 def narakeet_tts(text, language_code, voice):
+    args = [text,language_code,voice]
+    for a in args:
+        assert type(a) == str
     file_name_string, file_name_string_exists = file_path_audio(text, language_code)
     if (file_name_string_exists):
         return
     
-    command =  f"""narakeet-api-client --api-key {api_key} \
+    command =  f"""npx narakeet-api-client --api-key {api_key} \
  --project-type audio \
  --script "{text}" \
  --output-type mp3 \
  --voice {voice} \
  --output {file_name_string}"""
+
+    print(os.popen(command).read())
