ef23756f1f8587274348e4075c9d218905ac8181
JaredMathis <jared.mathis@native.consulting>
2023-01-01 05:07:25 -0500

    progress

--- tools/narakeet.py
+++ tools/narakeet.py
@@ -2,6 +2,8 @@ from common import file_json_read
 from gcloud import file_path_audio
 import os
 
+from tools.common import file_write
+
 api_key = file_json_read('./gitignore/narakeet-api-client.json')["api_key"]
 
 def narakeet_tts(text, language_code, voice):
@@ -11,14 +13,18 @@ def narakeet_tts(text, language_code, voice):
     file_name_string, file_name_string_exists = file_path_audio(text, language_code)
     if (file_name_string_exists):
         return
+
+    script_text_path = "gitignore/narakeet_script_text.txt"
+    file_write(script_text_path, text, cloud=False)
     
     command =  f"""npx narakeet-api-client --api-key {api_key} \
  --project-type audio \
- --script "{text}" \
+ --script-file {script_text_path} \
  --output-type mp3 \
  --voice {voice} \
  --output {file_name_string}"""
 
+    print(text)
     print(os.popen(command).read())
 
     exit()
