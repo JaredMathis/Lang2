190c501bbc2c0129c859d0135138a1cbf11feea5
JaredMathis <jared.mathis@native.consulting>
2023-01-02 16:13:48 -0500

    update

--- public/main.mjs
+++ public/main.mjs
@@ -260,7 +260,10 @@ async function screen_read_chapter(){
         let verse_number = span(verse_element_original, verse.verse);
         click(verse_number, () => {
             verse_toolbars.forEach(v => v.hidden = !v.hidden);
-            verse_toolbar.scrollIntoView();
+            // Give the browser a chance to render
+            setTimeout(() => {
+                verse_toolbar.scrollIntoView();
+            });
         })
         button_fifth(choose_verse_container, verse.verse, () => {
             verse_element.scrollIntoView();            
