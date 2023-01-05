95a09012e3176f5728df53b2adc636cbe86e60f6
JaredMathis <jared.mathis@native.consulting>
2023-01-05 16:11:59 -0500

    progress

--- public/main.mjs
+++ public/main.mjs
@@ -767,7 +767,7 @@ function screen_pre_quiz_generic(choice, screen_back, use_mistakes, noun) {
     text_words_low_high(choice, noun);
     button(document.body, 'Study', () => screen_study(choice, use_mistakes));
     if (category_selected === category_spelling) {
-        let sizes = [1,2,3];
+        let sizes = [3,2,1];
         for (let size_ of sizes) {
             let size = size_;
             button(document.body, `Quiz (${size} letter${size != 1 ? 's' : ''})`, () => {
