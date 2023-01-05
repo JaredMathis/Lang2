76e6b1ef13ffd8bf0daa43884d83976a37ebbe92
JaredMathis <jared.mathis@native.consulting>
2023-01-05 16:46:59 -0500

    progres

--- public/main.mjs
+++ public/main.mjs
@@ -764,8 +764,6 @@ function screen_category(choice) {
 function screen_pre_quiz_generic(choice, screen_back, use_mistakes, noun) {
     screen_home_non(screen_back);
 
-    debugger;
-
     text_words_low_high(choice, noun);
     button(document.body, 'Study', () => screen_study(choice, use_mistakes));
     if (!use_mistakes && category_selected === category_spelling) {
